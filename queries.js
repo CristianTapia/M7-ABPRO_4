a8efd41f9639a2e723560e2e07114a98d353497d
Cristian <ctapia.macaya@gmail.com>
2023-03-28 21:18:15 -0300

    ejemplo agregado

--- queries.js
+++ queries.js
@@ -3,13 +3,13 @@ import pool from "./lib/db_connection.js";
 pool.connect();
 
 async function queries() {
-    await pool.query('BEGIN');
-    try {
-        let resultado = await pool.query("SELECT * FROM transacciones");
-        console.table(resultado)
-    } catch(e) {
-        console.log(e);
-    }
+  // await pool.query('BEGIN');
+  try {
+    let resultado = await pool.query("SELECT * FROM usuarios");
+    console.table(resultado.rows);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 queries();
