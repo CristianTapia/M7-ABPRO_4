e53f323e3142d859d08856386a9e7f0ffcd087c4
Loreto_menddro <loretoalmendra.rc@gmail.com>
2023-03-28 21:41:11 -0300

    yargs

--- queries.js
+++ queries.js
@@ -1,4 +1,21 @@
 import pool from "./lib/db_connection.js";
+import pg from "pg";
+const { Pool } = pg;
+import { argv } from "node:process";
+import _yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+const yargs = _yargs(hideBin(process.argv));
+import dotenv from "dotenv";
+//dotenv.config();
+
+yargs.option({
+  n: { demandOption: false, alias: "descripcion" },
+  r: { demandOption: false, alias: "fecha" },
+  c: { demandOption: false, alias: "monto" },
+  l: { demandOption: false, alias: "cuenta" },
+});
+
+const params = yargs.argv;
 
 pool.connect();
 
