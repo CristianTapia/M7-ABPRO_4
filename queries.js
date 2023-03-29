73b3a32d0a74d27204fb3d4c5fdbb3c215bdf91d
Cristian <ctapia.macaya@gmail.com>
2023-03-28 21:03:21 -0300

    BD conectada

--- queries.js
+++ queries.js
@@ -1,1 +1,15 @@
-import pool from "./lib/db_connection";
+import pool from "./lib/db_connection.js";
+
+pool.connect();
+
+async function queries() {
+    await pool.query('BEGIN');
+    try {
+        let resultado = await pool.query("SELECT * FROM transacciones");
+        console.table(resultado)
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+queries();
