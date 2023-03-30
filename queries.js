0e8a04c1e89b442e8cccc147c97f73ab09a120d1
Loreto_menddro <loretoalmendra.rc@gmail.com>
2023-03-30 19:35:31 -0300

    punto 2 check

--- queries.js
+++ queries.js
@@ -1,5 +1,6 @@
 import pool from "./lib/db_connection.js";
 import pg from "pg";
+import cursor from "pg-cursor";
 const { Pool } = pg;
 import { argv } from "node:process";
 import _yargs from "yargs";
@@ -29,4 +30,16 @@ async function queries() {
   }
 }
 
-queries();
+
+async function query() {
+  try {
+    const cursor = await pool.query('SELECT * FROM transacciones WHERE id_cuenta=$1 limit 10', [1]);
+    console.table(cursor.rows)
+  } catch (err) {
+    console.log(err)
+  }
+
+}
+
+
+query()
