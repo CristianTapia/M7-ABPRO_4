8672949514f5230332199bc8a5c01878400006ce
LoretoAlmendro <loretoalmendra.rc@gmail.com>
2023-03-31 11:30:53 -0300

    punto 2 corregido

--- queries.js
+++ queries.js
@@ -1,7 +1,7 @@
 import pool from "./lib/db_connection.js";
 import pg from "pg";
 import cursor from "pg-cursor";
-const { Pool } = pg;
+const { Pool } = pg; 
 import { argv } from "node:process";
 import _yargs from "yargs";
 import { hideBin } from "yargs/helpers";
@@ -16,8 +16,35 @@ yargs.option({
 
 const params = yargs.argv;
 
-// pool.connect();
-const client = await pool.connect();
+
+
+async function queryCuenta(){
+  const client= await pool.connect()
+  const text= 'SELECT * FROM transacciones WHERE id_cuenta=$1 limit 10, [1]'
+  const values= [10]
+  const cursor= client.query(new cursor(text,values))
+  cursor.read(10, (err, rows) => {
+    cursor.close(() => {
+      client.release()
+    })
+  })
+
+
+
+}
+
+
+
+
+
+/* async function query() {
+  try {
+    const cursor = await pool.query();
+    console.table(cursor.rows)
+  } catch (err) {
+    console.log(err)
+  }
+} */
 
 async function queries(query, mensaje, ...params) {
   // await pool.query("BEGIN");
@@ -34,14 +61,7 @@ async function queries(query, mensaje, ...params) {
   }
 }
 
-async function query() {
-  try {
-    const cursor = await pool.query('SELECT * FROM transacciones WHERE id_cuenta=$1 limit 10', [1]);
-    console.table(cursor.rows)
-  } catch (err) {
-    console.log(err)
-  }
-}
+
 
 if (argv[2] == "insertar") {
   queries(
